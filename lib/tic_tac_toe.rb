0bd5f3a7f4c5a8d75bf43c040e782dd25e31b042
Lige <lige.williamson@gmail.com>
2019-03-29 22:44:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,5 +61,37 @@ def current_player(board)
   turn_count(board).odd? ? "Y" : "X"
 end
 
-def won?
+def won?(board)
+  x_indices = []
+  o_indices = []
+  win_status = false
+
+  board.each_index do |i|
+    x_indices << i if board[i] == "X"
+    o_indices << i if board[i] == "O"
+  end
+
+
+ WIN_COMBINATIONS.each do |combination|
+    win_status = combination if x_indices & combination == combination
+    win_status = combination if o_indices & combination == combination
+  end
+  win_status
+end
+
+def full?(board)
+  board.none?{|space| space === " "} && !won?(board)
+end
+
+def draw?(board)
+  full?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board)
+
+def winner(board)
+  winning_indices = won?(board)
+  winner = won?(board) ? board[winning_indices[0]] : nil
+  winner
 end
